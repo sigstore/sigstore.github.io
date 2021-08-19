ab74f781a18f5f924c14cc7875c6ea673148cbae
Mark Dunbavan <mdunbavan@gmail.com>
2021-08-19 10:21:44 +0100

    init docs commit
    Signed-off-by: Mark Dunbavan <mdunbavan@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,14 @@ import theme from '@nuxt/content-theme-docs'
 
 export default theme({
   docs: {
-    primaryColor: '#E24F55'
+    primaryColor: '#6349FF',
+    secondaryColor: '#C29G94',
+  },
+  build: {
+    babel:{
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }]
+      ]
+    }
   }
 })
